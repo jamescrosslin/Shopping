726288b8d3c6218da1da0c7ec8a1d479293d54fa
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-27 14:11:13 -0700

    fixed broken item addition

--- Shopping/additem.js
+++ Shopping/additem.js
@@ -1,242 +1,225 @@
+const lookup = {};
+
 function additem() {
-    var itemType = document.getElementById("itemType");
-    var item = document.getElementById("itemChoose");
-    if (item.value === "DefaultItem") {
-        alert("Please select the item you want to add!");
-    }
-    else if (itemType.value === "f") {
-        addfood();
-    }
-    else if (itemType.value === "cl") {
-        addclothes();
-    }
-    else if (itemType.value === "tch") {
-        addtech();
-    }
-    else if (itemType.value === "c") {
-        addcars();
-    }
-    else if (itemType.value === "l") {
-        addlibrary();
-    }
-    else if (itemType.value === "o") {
-        addother();
-    }
+  var itemType = document.getElementById('itemType');
+  var item = document.getElementById('itemChoose');
+
+  //
+
+  if (item.value === 'DefaultItem') {
+    alert('Please select the item you want to add!');
+  } else if (itemType.value === 'f') {
+    addfood();
+  } else if (itemType.value === 'cl') {
+    addclothes();
+  } else if (itemType.value === 'tch') {
+    addtech();
+  } else if (itemType.value === 'c') {
+    addcars();
+  } else if (itemType.value === 'l') {
+    addlibrary();
+  } else if (itemType.value === 'o') {
+    addother();
+  }
 }
+
+function processItem(itemType, itemChoice) {}
+
 function addfood() {
-    var item = document.getElementById("itemChoose");
-    var foodAdded = document.getElementById("foodAdded");
-    var foodText = item.options[item.selectedIndex].text;
+  var item = document.getElementById('itemChoose');
+  var foodAdded = document.getElementById('foodAdded');
+  var foodText = item.options[item.selectedIndex].text;
 
-    var paragraph = document.createElement("p");
-    var ptxt = document.createTextNode(foodText);
-    paragraph.id = foodText;
-    paragraph.appendChild(ptxt);
+  var paragraph = document.createElement('p');
+  var ptxt = document.createTextNode(foodText);
+  paragraph.id = foodText;
+  paragraph.appendChild(ptxt);
 
-    var itemtxt = foodAdded.getElementsByTagName("p");
-    var sameValue;
+  var itemtxt = foodAdded.getElementsByTagName('p');
 
-    for (i = 0; i < itemtxt.length; i++) {
-        sameValue = itemtxt[i].getAttribute("id");
+  for (i = 0; i < itemtxt.length; i++) {
+    const sameValue = itemtxt[i].getAttribute('id');
+    if (sameValue === foodText) {
+      var multiplier = document.getElementById(sameValue);
+      var multiregex = /[a-zA-Z]+ x([2-4])/;
+      if (multiregex.test(multiplier.innerHTML)) {
+        let y = multiregex.exec(multiplier.innerHTML);
+        var z = parseInt(y[1]) + 1;
+        return (multiplier.innerHTML = sameValue + ' x' + z);
+      } else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
+        return alert('You can not add more than 5 items of ' + sameValue + 's');
+      } else {
+        var x = 2;
+        return (multiplier.innerHTML = sameValue + ' x' + x);
+      }
     }
-    if (sameValue == foodText) {
-        var multiplier = document.getElementById(sameValue);
-        var multiregex = /[a-zA-Z]+ x([2-4])/;
-        if (multiregex.test(multiplier.innerHTML)) {
-            let y = multiregex.exec(multiplier.innerHTML);
-            var z = parseInt(y[1]) + 1;
-            multiplier.innerHTML = sameValue + " x" + z;
-        }
-        else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
-            alert("You can not add more than 5 items of " + sameValue + "s");
-        }
-        else {
-            var x = 2;
-            multiplier.innerHTML = sameValue + " x" + x;
-        }
-    }
-    else {
-        foodAdded.appendChild(paragraph);
-    }
+  }
+
+  foodAdded.appendChild(paragraph);
 }
 function addclothes() {
-    var item = document.getElementById("itemChoose");
-    var clothesAdded = document.getElementById("clothesAdded");
-    var clothesText = item.options[item.selectedIndex].text;
+  var item = document.getElementById('itemChoose');
+  var clothesAdded = document.getElementById('clothesAdded');
+  var clothesText = item.options[item.selectedIndex].text;
 
-    var paragraph = document.createElement("p");
-    var ptxt = document.createTextNode(clothesText);
+  var paragraph = document.createElement('p');
+  var ptxt = document.createTextNode(clothesText);
 
-    paragraph.id = clothesText;
-    paragraph.appendChild(ptxt);
+  paragraph.id = clothesText;
+  paragraph.appendChild(ptxt);
 
-    var itemtxt = clothesAdded.getElementsByTagName("p");
-    var sameValue;
+  var itemtxt = clothesAdded.getElementsByTagName('p');
+  var sameValue;
 
-    for (i = 0; i < itemtxt.length; i++) {
-        sameValue = itemtxt[i].getAttribute("id");
+  for (i = 0; i < itemtxt.length; i++) {
+    sameValue = itemtxt[i].getAttribute('id');
+  }
+  if (sameValue == clothesText) {
+    var multiplier = document.getElementById(sameValue);
+    var multiregex = /[a-zA-Z]+ x([2-4])/;
+    if (multiregex.test(multiplier.innerHTML)) {
+      let y = multiregex.exec(multiplier.innerHTML);
+      var z = parseInt(y[1]) + 1;
+      multiplier.innerHTML = sameValue + ' x' + z;
+    } else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
+      alert('You can not add more than 5 items of ' + sameValue + 's');
+    } else {
+      var x = 2;
+      multiplier.innerHTML = sameValue + ' x' + x;
     }
-    if (sameValue == clothesText) {
-        var multiplier = document.getElementById(sameValue);
-        var multiregex = /[a-zA-Z]+ x([2-4])/;
-        if (multiregex.test(multiplier.innerHTML)) {
-            let y = multiregex.exec(multiplier.innerHTML);
-            var z = parseInt(y[1]) + 1;
-            multiplier.innerHTML = sameValue + " x" + z;
-        }
-        else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
-            alert("You can not add more than 5 items of " + sameValue + "s");
-        }
-        else {
-            var x = 2;
-            multiplier.innerHTML = sameValue + " x" + x;
-        }
-    }
-    else {
-        clothesAdded.appendChild(paragraph);
-    }
+  } else {
+    clothesAdded.appendChild(paragraph);
+  }
 }
 function addtech() {
-    var item = document.getElementById("itemChoose");
-    var techAdded = document.getElementById("techAdded");
-    var techText = item.options[item.selectedIndex].text;
+  var item = document.getElementById('itemChoose');
+  var techAdded = document.getElementById('techAdded');
+  var techText = item.options[item.selectedIndex].text;
 
-    var paragraph = document.createElement("p");
-    var ptxt = document.createTextNode(techText);
-    paragraph.id = techText;
-    paragraph.appendChild(ptxt);
+  var paragraph = document.createElement('p');
+  var ptxt = document.createTextNode(techText);
+  paragraph.id = techText;
+  paragraph.appendChild(ptxt);
 
-    var itemtxt = techAdded.getElementsByTagName("p");
-    var sameValue;
+  var itemtxt = techAdded.getElementsByTagName('p');
+  var sameValue;
 
-    for (i = 0; i < itemtxt.length; i++) {
-        sameValue = itemtxt[i].getAttribute("id");
+  for (i = 0; i < itemtxt.length; i++) {
+    sameValue = itemtxt[i].getAttribute('id');
+  }
+  if (sameValue == techText) {
+    var multiplier = document.getElementById(sameValue);
+    var multiregex = /[a-zA-Z]+ x([2-4])/;
+    if (multiregex.test(multiplier.innerHTML)) {
+      let y = multiregex.exec(multiplier.innerHTML);
+      var z = parseInt(y[1]) + 1;
+      multiplier.innerHTML = sameValue + ' x' + z;
+    } else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
+      alert('You can not add more than 5 items of ' + sameValue + 's');
+    } else {
+      var x = 2;
+      multiplier.innerHTML = sameValue + ' x' + x;
     }
-    if (sameValue == techText) {
-        var multiplier = document.getElementById(sameValue);
-        var multiregex = /[a-zA-Z]+ x([2-4])/;
-        if (multiregex.test(multiplier.innerHTML)) {
-            let y = multiregex.exec(multiplier.innerHTML);
-            var z = parseInt(y[1]) + 1;
-            multiplier.innerHTML = sameValue + " x" + z;
-        }
-        else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
-            alert("You can not add more than 5 items of " + sameValue + "s");
-        }
-        else {
-            var x = 2;
-            multiplier.innerHTML = sameValue + " x" + x;
-        }
-    }
-    else {
-        techAdded.appendChild(paragraph);
-    }
+  } else {
+    techAdded.appendChild(paragraph);
+  }
 }
 function addcars() {
-    var item = document.getElementById("itemChoose");
-    var carsAdded = document.getElementById("carsAdded");
-    var carsText = item.options[item.selectedIndex].text;
+  var item = document.getElementById('itemChoose');
+  var carsAdded = document.getElementById('carsAdded');
+  var carsText = item.options[item.selectedIndex].text;
 
-    var paragraph = document.createElement("p");
-    var ptxt = document.createTextNode(carsText);
-    paragraph.id = carsText;
-    paragraph.appendChild(ptxt);
+  var paragraph = document.createElement('p');
+  var ptxt = document.createTextNode(carsText);
+  paragraph.id = carsText;
+  paragraph.appendChild(ptxt);
 
-    var itemtxt = carsAdded.getElementsByTagName("p");
-    var sameValue;
+  var itemtxt = carsAdded.getElementsByTagName('p');
+  var sameValue;
 
-    for (i = 0; i < itemtxt.length; i++) {
-        sameValue = itemtxt[i].getAttribute("id");
+  for (i = 0; i < itemtxt.length; i++) {
+    sameValue = itemtxt[i].getAttribute('id');
+  }
+  if (sameValue == carsText) {
+    var multiplier = document.getElementById(sameValue);
+    var multiregex = /[a-zA-Z]+ x([2-4])/;
+    if (multiregex.test(multiplier.innerHTML)) {
+      let y = multiregex.exec(multiplier.innerHTML);
+      var z = parseInt(y[1]) + 1;
+      multiplier.innerHTML = sameValue + ' x' + z;
+    } else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
+      alert('You can not add more than 5 items of ' + sameValue + 's');
+    } else {
+      var x = 2;
+      multiplier.innerHTML = sameValue + ' x' + x;
     }
-    if (sameValue == carsText) {
-        var multiplier = document.getElementById(sameValue);
-        var multiregex = /[a-zA-Z]+ x([2-4])/;
-        if (multiregex.test(multiplier.innerHTML)) {
-            let y = multiregex.exec(multiplier.innerHTML);
-            var z = parseInt(y[1]) + 1;
-            multiplier.innerHTML = sameValue + " x" + z;
-        }
-        else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
-            alert("You can not add more than 5 items of " + sameValue + "s");
-        }
-        else {
-            var x = 2;
-            multiplier.innerHTML = sameValue + " x" + x;
-        }
-    }
-    else {
-        carsAdded.appendChild(paragraph);
-    }
+  } else {
+    carsAdded.appendChild(paragraph);
+  }
 }
 function addlibrary() {
-    var item = document.getElementById("itemChoose");
-    var libraryAdded = document.getElementById("libraryAdded");
-    var libraryText = item.options[item.selectedIndex].text;
+  var item = document.getElementById('itemChoose');
+  var libraryAdded = document.getElementById('libraryAdded');
+  var libraryText = item.options[item.selectedIndex].text;
 
-    var paragraph = document.createElement("p");
-    var ptxt = document.createTextNode(libraryText);
-    paragraph.id = libraryText;
-    paragraph.appendChild(ptxt);
+  var paragraph = document.createElement('p');
+  var ptxt = document.createTextNode(libraryText);
+  paragraph.id = libraryText;
+  paragraph.appendChild(ptxt);
 
-    var itemtxt = libraryAdded.getElementsByTagName("p");
-    var sameValue;
+  var itemtxt = libraryAdded.getElementsByTagName('p');
+  var sameValue;
 
-    for (i = 0; i < itemtxt.length; i++) {
-        sameValue = itemtxt[i].getAttribute("id");
+  for (i = 0; i < itemtxt.length; i++) {
+    sameValue = itemtxt[i].getAttribute('id');
+  }
+  if (sameValue == libraryText) {
+    var multiplier = document.getElementById(sameValue);
+    var multiregex = /[a-zA-Z]+ x([2-4])/;
+    if (multiregex.test(multiplier.innerHTML)) {
+      let y = multiregex.exec(multiplier.innerHTML);
+      var z = parseInt(y[1]) + 1;
+      multiplier.innerHTML = sameValue + ' x' + z;
+    } else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
+      alert('You can not add more than 5 items of ' + sameValue + 's');
+    } else {
+      var x = 2;
+      multiplier.innerHTML = sameValue + ' x' + x;
     }
-    if (sameValue == libraryText) {
-        var multiplier = document.getElementById(sameValue);
-        var multiregex = /[a-zA-Z]+ x([2-4])/;
-        if (multiregex.test(multiplier.innerHTML)) {
-            let y = multiregex.exec(multiplier.innerHTML);
-            var z = parseInt(y[1]) + 1;
-            multiplier.innerHTML = sameValue + " x" + z;
-        }
-        else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
-            alert("You can not add more than 5 items of " + sameValue + "s");
-        }
-        else {
-            var x = 2;
-            multiplier.innerHTML = sameValue + " x" + x;
-        }
-    }
-    else {
-        libraryAdded.appendChild(paragraph);
-    }
+  } else {
+    libraryAdded.appendChild(paragraph);
+  }
 }
 function addother() {
-    var item = document.getElementById("itemChoose");
-    var otherAdded = document.getElementById("otherAdded");
-    var otherText = item.options[item.selectedIndex].text;
+  var item = document.getElementById('itemChoose');
+  var otherAdded = document.getElementById('otherAdded');
+  var otherText = item.options[item.selectedIndex].text;
 
-    var paragraph = document.createElement("p");
-    var ptxt = document.createTextNode(otherText);
-    paragraph.id = otherText;
-    paragraph.appendChild(ptxt);
+  var paragraph = document.createElement('p');
+  var ptxt = document.createTextNode(otherText);
+  paragraph.id = otherText;
+  paragraph.appendChild(ptxt);
 
-    var itemtxt = otherAdded.getElementsByTagName("p");
-    var sameValue;
+  var itemtxt = otherAdded.getElementsByTagName('p');
+  var sameValue;
 
-    for (i = 0; i < itemtxt.length; i++) {
-        sameValue = itemtxt[i].getAttribute("id");
+  for (i = 0; i < itemtxt.length; i++) {
+    sameValue = itemtxt[i].getAttribute('id');
+  }
+  if (sameValue == otherText) {
+    var multiplier = document.getElementById(sameValue);
+    var multiregex = /[a-zA-Z]+ x([2-4])/;
+    if (multiregex.test(multiplier.innerHTML)) {
+      let y = multiregex.exec(multiplier.innerHTML);
+      var z = parseInt(y[1]) + 1;
+      multiplier.innerHTML = sameValue + ' x' + z;
+    } else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
+      alert('You can not add more than 5 items of ' + sameValue + 's');
+    } else {
+      var x = 2;
+      multiplier.innerHTML = sameValue + ' x' + x;
     }
-    if (sameValue == otherText) {
-        var multiplier = document.getElementById(sameValue);
-        var multiregex = /[a-zA-Z]+ x([2-4])/;
-        if (multiregex.test(multiplier.innerHTML)) {
-            let y = multiregex.exec(multiplier.innerHTML);
-            var z = parseInt(y[1]) + 1;
-            multiplier.innerHTML = sameValue + " x" + z;
-        }
-        else if (/[a-zA-Z]+ x5/.test(multiplier.innerHTML)) {
-            alert("You can not add more than 5 items of " + sameValue + "s");
-        }
-        else {
-            var x = 2;
-            multiplier.innerHTML = sameValue + " x" + x;
-        }
-    }
-    else {
-        otherAdded.appendChild(paragraph);
-    }
+  } else {
+    otherAdded.appendChild(paragraph);
+  }
 }
--- Shopping/search.js
+++ Shopping/search.js
@@ -1,94 +1,152 @@
 function mainfunction() {
-    var itemChoose = document.getElementById("itemChoose");
-    var itemType = document.getElementById("itemType");
-    itemType.addEventListener('click', selectItem);
-    itemChoose.addEventListener('click', function () {
-        if (itemType.value === "DefaultType") {
-            alert("Please choose the type of item you want to buy before choosing the item!");
-            itemType.focus();
-        }
-    });
+  var itemChoose = document.getElementById('itemChoose');
+  var itemType = document.getElementById('itemType');
+  itemType.addEventListener('change', selectItem);
+  itemChoose.addEventListener('click', function () {
+    if (itemType.value === 'DefaultType') {
+      alert('Please choose the type of item you want to buy before choosing the item!');
+      itemType.focus();
+    }
+  });
 }
 function selectItem() {
-    var itemType = document.getElementById("itemType");
-    var itemChoose = document.getElementById("itemChoose");
-    
-    if (itemType.value === "f") {
-        while (itemChoose.length > 1) {
-            itemChoose.removeChild(itemChoose.lastChild);
-        }
-        var foodItems = ["Hamburger", "Pizza", "Coffee", "Chicken Nuggets", "Bread", "Tea", "Soda", "Cake", "Ice Cream"];
-        for (i=0; i<foodItems.length; i++) {
-            var option = document.createElement("option");
-            option.setAttribute('value', 'food'+i);
-            var optiontxt = document.createTextNode(foodItems[i]);
-            option.appendChild(optiontxt);
-            itemChoose.appendChild(option);
-        }
+  var itemType = document.getElementById('itemType');
+  var itemChoose = document.getElementById('itemChoose');
+
+  if (itemType.value === 'f') {
+    while (itemChoose.length > 1) {
+      itemChoose.removeChild(itemChoose.lastChild);
     }
-    if (itemType.value === "cl") {
-        while (itemChoose.length > 1) {
-            itemChoose.removeChild(itemChoose.lastChild);
-        }
-        var clothesItems = ["T-Shirt", "Coat", "Sweater", "Jacket", "Shorts", "Jeans", "Trousers", "Shoes", "Socks"];
-        for (i=0; i<clothesItems.length; i++) {
-            var option = document.createElement("option");
-            option.setAttribute('value', 'clothes'+i);
-            var optiontxt = document.createTextNode(clothesItems[i]);
-            option.appendChild(optiontxt);
-            itemChoose.appendChild(option);
-        }
+    var foodItems = [
+      'Hamburger',
+      'Pizza',
+      'Coffee',
+      'Chicken Nuggets',
+      'Bread',
+      'Tea',
+      'Soda',
+      'Cake',
+      'Ice Cream',
+    ];
+    for (i = 0; i < foodItems.length; i++) {
+      var option = document.createElement('option');
+      option.setAttribute('value', 'food' + i);
+      var optiontxt = document.createTextNode(foodItems[i]);
+      option.appendChild(optiontxt);
+      itemChoose.appendChild(option);
     }
-    if (itemType.value === "tch") {
-        while (itemChoose.length > 1) {
-            itemChoose.removeChild(itemChoose.lastChild);
-        }
-        var techItems = ["Desktop PC", "Laptop", "Processor", "Graphic Card", "Ram", "iPhone", "Samsung", "Television", "Radio", "Speakers"];
-        for (i=0; i<techItems.length; i++) {
-            var option = document.createElement("option");
-            option.setAttribute('value', 'clothes'+i);
-            var optiontxt = document.createTextNode(techItems[i]);
-            option.appendChild(optiontxt);
-            itemChoose.appendChild(option);
-        }
+  }
+  if (itemType.value === 'cl') {
+    while (itemChoose.length > 1) {
+      itemChoose.removeChild(itemChoose.lastChild);
     }
-    if (itemType.value === "c") {
-        while (itemChoose.length > 1) {
-            itemChoose.removeChild(itemChoose.lastChild);
-        }
-        var carsItems = ["Audi", "BMW", "Volkswagen", "Mercedes", "Porsche", "Ferrari", "Lamborghini", "Bugatti"];
-        for (i=0; i<carsItems.length; i++) {
-            var option = document.createElement("option");
-            option.setAttribute('value', 'clothes'+i);
-            var optiontxt = document.createTextNode(carsItems[i]);
-            option.appendChild(optiontxt);
-            itemChoose.appendChild(option);
-        }
+    var clothesItems = [
+      'T-Shirt',
+      'Coat',
+      'Sweater',
+      'Jacket',
+      'Shorts',
+      'Jeans',
+      'Trousers',
+      'Shoes',
+      'Socks',
+    ];
+    for (i = 0; i < clothesItems.length; i++) {
+      var option = document.createElement('option');
+      option.setAttribute('value', 'clothes' + i);
+      var optiontxt = document.createTextNode(clothesItems[i]);
+      option.appendChild(optiontxt);
+      itemChoose.appendChild(option);
     }
-    if (itemType.value === "l") {
-        while (itemChoose.length > 1) {
-            itemChoose.removeChild(itemChoose.lastChild);
-        }
-        var libraryItems = ["Book", "Notebook", "Magazine", "Pencil", "Pen", "Rubber", "Ruler", "Colour Pen"];
-        for (i=0; i<libraryItems.length; i++) {
-            var option = document.createElement("option");
-            option.setAttribute('value', 'clothes'+i);
-            var optiontxt = document.createTextNode(libraryItems[i]);
-            option.appendChild(optiontxt);
-            itemChoose.appendChild(option);
-        }
+  }
+  if (itemType.value === 'tch') {
+    while (itemChoose.length > 1) {
+      itemChoose.removeChild(itemChoose.lastChild);
     }
-    if (itemType.value === "o") {
-        while (itemChoose.length > 1) {
-            itemChoose.removeChild(itemChoose.lastChild);
-        }
-        var otherItems = ["Lawn Mower", "Ball", "Tennis Racket", "Shovel", "Vase", "Couch", "Chair", "Blanket", ];
-        for (i=0; i<otherItems.length; i++) {
-            var option = document.createElement("option");
-            option.setAttribute('value', 'clothes'+i);
-            var optiontxt = document.createTextNode(otherItems[i]);
-            option.appendChild(optiontxt);
-            itemChoose.appendChild(option);
-        }
+    var techItems = [
+      'Desktop PC',
+      'Laptop',
+      'Processor',
+      'Graphic Card',
+      'Ram',
+      'iPhone',
+      'Samsung',
+      'Television',
+      'Radio',
+      'Speakers',
+    ];
+    for (i = 0; i < techItems.length; i++) {
+      var option = document.createElement('option');
+      option.setAttribute('value', 'clothes' + i);
+      var optiontxt = document.createTextNode(techItems[i]);
+      option.appendChild(optiontxt);
+      itemChoose.appendChild(option);
     }
+  }
+  if (itemType.value === 'c') {
+    while (itemChoose.length > 1) {
+      itemChoose.removeChild(itemChoose.lastChild);
+    }
+    var carsItems = [
+      'Audi',
+      'BMW',
+      'Volkswagen',
+      'Mercedes',
+      'Porsche',
+      'Ferrari',
+      'Lamborghini',
+      'Bugatti',
+    ];
+    for (i = 0; i < carsItems.length; i++) {
+      var option = document.createElement('option');
+      option.setAttribute('value', 'clothes' + i);
+      var optiontxt = document.createTextNode(carsItems[i]);
+      option.appendChild(optiontxt);
+      itemChoose.appendChild(option);
+    }
+  }
+  if (itemType.value === 'l') {
+    while (itemChoose.length > 1) {
+      itemChoose.removeChild(itemChoose.lastChild);
+    }
+    var libraryItems = [
+      'Book',
+      'Notebook',
+      'Magazine',
+      'Pencil',
+      'Pen',
+      'Rubber',
+      'Ruler',
+      'Colour Pen',
+    ];
+    for (i = 0; i < libraryItems.length; i++) {
+      var option = document.createElement('option');
+      option.setAttribute('value', 'clothes' + i);
+      var optiontxt = document.createTextNode(libraryItems[i]);
+      option.appendChild(optiontxt);
+      itemChoose.appendChild(option);
+    }
+  }
+  if (itemType.value === 'o') {
+    while (itemChoose.length > 1) {
+      itemChoose.removeChild(itemChoose.lastChild);
+    }
+    var otherItems = [
+      'Lawn Mower',
+      'Ball',
+      'Tennis Racket',
+      'Shovel',
+      'Vase',
+      'Couch',
+      'Chair',
+      'Blanket',
+    ];
+    for (i = 0; i < otherItems.length; i++) {
+      var option = document.createElement('option');
+      option.setAttribute('value', 'clothes' + i);
+      var optiontxt = document.createTextNode(otherItems[i]);
+      option.appendChild(optiontxt);
+      itemChoose.appendChild(option);
+    }
+  }
 }
